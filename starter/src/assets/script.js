cd2902607f4faa298274ca0998d6482d3c99e43f
Michael <michaelkorostilev@gmail.com>
2022-07-31 02:47:39 +0300

    cleaned code

--- starter/src/assets/script.js
+++ starter/src/assets/script.js
@@ -1,4 +1,3 @@
-/* Create an array named products which you will use to add all of your product object literals that you create in the next step. */
 
 let products = [
   {
@@ -23,29 +22,11 @@ let products = [
     image: "../images/orange.jpg",
   },
 ];
-/* Create 3 or more product objects using object literal notation 
-   Each product should include five properties
-   - name: name of product (string)
-   - price: price of product (number)
-   - quantity: quantity in cart should start at zero (number)
-   - productId: unique id for the product (number)
-   - image: picture of product (url string)
-*/
 
-/* Images provided in /images folder. All images from Unsplash.com
-   - cherry.jpg by Mae Mu
-   - orange.jpg by Mae Mu
-   - strawberry.jpg by Allec Gomes
-*/
 
-/* Declare an empty array named cart to hold the items in the cart */
 let cart = [];
 
-/* Create a function named addProductToCart that takes in the product productId as an argument
-- addProductToCart should get the correct product based on the productId
-- addProductToCart should then increase the product's quantity
-- if the product is not already in the cart, add it to the cart
-*/
+
 function addProductToCart(productId) {
   for (let i = 0; i < products.length; i++) {
     if (productId === products[i].productId) {
@@ -60,10 +41,6 @@ function addProductToCart(productId) {
   console.log(cart);
 }
 
-/* Create a function named increaseQuantity that takes in the productId as an argument
-  - increaseQuantity should get the correct product based on the productId
-  - increaseQuantity should then increase the product's quantity
-*/
 
 function increaseQuantity(productId) {
   for (let i = 0; i < products.length; i++) {
@@ -73,11 +50,6 @@ function increaseQuantity(productId) {
   }
 }
 
-/* Create a function named decreaseQuantity that takes in the productId as an argument
-  - decreaseQuantity should get the correct product based on the productId
-  - decreaseQuantity should decrease the quantity of the product
-  - if the function decreases the quantity to 0, the product is removed from the cart
-*/
 
 function decreaseQuantity(productId) {
   for (let i = 0; i < products.length; i++) {
@@ -89,22 +61,8 @@ function decreaseQuantity(productId) {
     } 
   }
 }
-// function decreaseQuantity(productId) {
-//   for (let i = 0; i < products.length; i++) {
-//     if (productId === products[i].productId && products[i].quantity > 1) {
-//       products[i].quantity--;
-//     } else if (productId === products[i].productId && products[i].quantity <= 1) {
-//       products[i].quantity--;
-//       removeProductFromCart(products[i])
-//     }
-//   }
-// }
 
-/* Create a function named removeProductFromCart that takes in the productId as an argument
-  - removeProductFromCart should get the correct product based on the productId
-  - removeProductFromCart should update the product quantity to 0
-  - removeProductFromCart should remove the product from the cart
-*/
+
 function removeProductFromCart(productId) {
   for (let i = 0; i < products.length; i++) {
     if (productId === products[i].productId) {
@@ -114,10 +72,6 @@ function removeProductFromCart(productId) {
   }
 }
 
-/* Create a function named cartTotal that has no parameters
-  - cartTotal should iterate through the cart to get the total of all products
-  - cartTotal should return the sum of the products in the cart
-*/
 
 function cartTotal() {
   let sum = 0;
@@ -128,7 +82,6 @@ function cartTotal() {
   return sum;
 }
 
-/* Create a function called emptyCart that empties the products from the cart */
 
 function emptyCart() {
   cart = [];
@@ -137,18 +90,12 @@ function emptyCart() {
   });
 }
 
-/* Create a function named pay that takes in an amount as an argument
-  - pay will return a negative number if there is a remaining balance
-  - pay will return a positive number if money should be returned to customer
-*/
-
 function pay(amount) {
   let balance = 0;
   balance = cartTotal();
   return  amount - balance;
 }
 
-/* Place stand out suggestions here (stand out suggestions can be found at the bottom of the project rubric.)*/
 
 /* The following is for running unit tests. 
    To fully complete this project, it is expected that all tests pass.
