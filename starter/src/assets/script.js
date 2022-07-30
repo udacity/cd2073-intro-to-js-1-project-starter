d7886894cebc810699493f7a6c47c4b930704906
Michael <michaelkorostilev@gmail.com>
2022-07-30 17:13:56 +0300

    added addProductCart

--- starter/src/assets/script.js
+++ starter/src/assets/script.js
@@ -1,6 +1,28 @@
 /* Create an array named products which you will use to add all of your product object literals that you create in the next step. */
 
-
+let products = [
+  {
+    name: "Carton of Cherries",
+    price: 4,
+    quantity: 0,
+    productId: 1,
+    image: "../images/cherry.jpg",
+  },
+  {
+    name: "Carton of Strawberries",
+    price: 5,
+    quantity: 0,
+    productId: 2,
+    image: "../images/strawberry.jpg",
+  },
+  {
+    name: "Carton of Cherries",
+    price: 4,
+    quantity: 0,
+    productId: 3,
+    image: "../images/orange.jpg",
+  },
+];
 /* Create 3 or more product objects using object literal notation 
    Each product should include five properties
    - name: name of product (string)
@@ -17,17 +39,36 @@
 */
 
 /* Declare an empty array named cart to hold the items in the cart */
+let cart = [];
+
 
 /* Create a function named addProductToCart that takes in the product productId as an argument
-  - addProductToCart should get the correct product based on the productId
-  - addProductToCart should then increase the product's quantity
-  - if the product is not already in the cart, add it to the cart
+- addProductToCart should get the correct product based on the productId
+- addProductToCart should then increase the product's quantity
+- if the product is not already in the cart, add it to the cart
 */
+function addProductToCart(productId) {
+  for (let i = 0; i < products.length; i++) {
+    if (productId === products[i].productId) {
+      if (cart.includes(products[i])) {
+        products[i].quantity++;    
+      }else {
+        cart.push(products[i])
+        products[i].quantity++;
+      }
+    }
+  }
+  console.log(cart)
+}
 
 /* Create a function named increaseQuantity that takes in the productId as an argument
   - increaseQuantity should get the correct product based on the productId
   - increaseQuantity should then increase the product's quantity
 */
+
+function increaseQuantity(productId) {
+
+}
 
 /* Create a function named decreaseQuantity that takes in the productId as an argument
   - decreaseQuantity should get the correct product based on the productId
@@ -35,25 +76,46 @@
   - if the function decreases the quantity to 0, the product is removed from the cart
 */
 
+function decreaseQuantity(productId) {
+  
+}
+
 /* Create a function named removeProductFromCart that takes in the productId as an argument
   - removeProductFromCart should get the correct product based on the productId
   - removeProductFromCart should update the product quantity to 0
   - removeProductFromCart should remove the product from the cart
 */
+function removeProductFromCart (productId) {
+
+}
+
 
 /* Create a function named cartTotal that has no parameters
   - cartTotal should iterate through the cart to get the total of all products
   - cartTotal should return the sum of the products in the cart
 */
 
+function cartTotal () {
+
+}
+
 /* Create a function called emptyCart that empties the products from the cart */
+
+function emptyCart() {
+
+}
 
 /* Create a function named pay that takes in an amount as an argument
   - pay will return a negative number if there is a remaining balance
   - pay will return a positive number if money should be returned to customer
 */
 
+function pay(amount) {
+
+}
+
 /* Place stand out suggestions here (stand out suggestions can be found at the bottom of the project rubric.)*/
+
 
 
 /* The following is for running unit tests. 
@@ -63,15 +125,15 @@
 */
 
 module.exports = {
-   products,
-   cart,
-   addProductToCart,
-   increaseQuantity,
-   decreaseQuantity,
-   removeProductFromCart,
-   cartTotal,
-   pay, 
-   emptyCart,
-   /* Uncomment the following line if completing the currency converter bonus */
-   // currency
-}
+  products,
+  cart,
+  addProductToCart,
+  increaseQuantity,
+  decreaseQuantity,
+  removeProductFromCart,
+  cartTotal,
+  pay,
+  emptyCart,
+  /* Uncomment the following line if completing the currency converter bonus */
+  // currency
+};
